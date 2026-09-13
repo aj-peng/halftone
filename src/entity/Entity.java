0c422c56986804fbda60d9cabd97f7efeebce43f
ajpeng <andrewjpeng@gmail.com>
2026-09-13 10:02:33 -0400

    Created Entity

---
+++ src/entity/Entity.java
@@ -0,0 +1,63 @@
+package entity;
+
+import main.GamePanel;
+import main.Utility;
+
+import javax.imageio.ImageIO;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Objects;
+
+public class Entity {
+    GamePanel gp;
+    Utility utility = new Utility();
+
+    public enum DIRECTION {UP, DOWN, LEFT, RIGHT}
+    public DIRECTION direction = DIRECTION.DOWN;
+
+    public int worldX, worldY, speed;
+    public boolean collision = false;
+    boolean idle = true;
+
+    public Rectangle hitbox = new Rectangle(0, 0, 48, 48);
+    int hitboxDefaultX, hitboxDefaultY;
+
+    int spriteNum = 0;
+    int spriteCounter = 0;
+    static final int spriteInterval = 15;
+    HashMap<DIRECTION, BufferedImage[]> spriteImage = new HashMap<>();
+
+    public Entity(GamePanel gp) {
+        this.gp = gp;
+    }
+
+    public void draw(Graphics2D g2) {
+        // no implementation
+    }
+
+    public void update() {
+        // no implementation
+    }
+
+    BufferedImage loadImage(String imagePath, int width, int height) {
+        BufferedImage image;
+        try {
+            image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(imagePath + ".png")));
+            image = utility.scaleImage(image, width, height);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return image;
+    }
+
+    BufferedImage getSpriteImage() {
+        BufferedImage[] images = spriteImage.get(direction);
+        if (images != null) {
+            return images[idle ? 0 : spriteNum];
+        }
+        return null;
+    }
+}
