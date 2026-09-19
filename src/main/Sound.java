c6d33fc58f0d707d8760b39593c31affd4b62c87
ajpeng <andrewjpeng@gmail.com>
2026-09-18 20:42:21 -0400

    Added chime01

--- src/main/Sound.java
+++ src/main/Sound.java
@@ -14,6 +14,7 @@ public class Sound {
 
     public Sound() {
         soundURL[0] = getClass().getResource("/sound/wet_caves.wav");
+        soundURL[1] = getClass().getResource("/sound/chime01.wav");
     }
 
     public void setFile(int index) {
